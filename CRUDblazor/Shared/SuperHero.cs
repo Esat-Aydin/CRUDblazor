b6e91ec26143821b643333411b28542324b51b44
Esat-Aydin <esat61_2001@hotmail.com>
2022-09-09 23:11:20 +0200

    superhero mdoel

---
+++ CRUDblazor/Shared/SuperHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDblazor.Shared
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string HeroName { get; s set; } = string.Empty;
+        public Comic Comic { get; set; }
+        public int ComicId { get; set; }
+        
+    }
+}
